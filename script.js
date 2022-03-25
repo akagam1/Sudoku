da445c38dbc387dfe8b970ebe2999360a0eadc85
arjun <arjunputhli00@gmail.com>
2022-03-25 17:37:04 +0530

    click hover effect added

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let grid = document.getElementById("board");
-cells = [];
+cell_grid = [];
 
 class Cell {
-    constructor(value=0,square){
+    constructor(value=0,box){
         this.value = value
-        this.square = square
+        this.box = box
     }
 }
 
@@ -14,20 +14,37 @@ function drawGrid(loop=9){
         for (let j = 0; j<loop; j++) {
             tempElement = document.createElement('div');
             tempRow[j] = new Cell(0,tempElement)
-            tempRow[j].square.classList.add('square');
-            tempRow[j].square.style.width = "60px";
-            tempRow[j].square.style.height = "60px";
-            tempRow[j].square.style.border = "2px solid #000000";
+            tempRow[j].box.classList.add('square');
+            tempRow[j].box.style.width = "60px";
+            tempRow[j].box.style.height = "60px";
+            tempRow[j].box.style.border = "2px solid #000000";
             if (j==2 || j==5) {
-                tempRow[j].square.style.borderRightColor = "#ff0000";
+                tempRow[j].box.style.borderRightColor = "#ff0000";
+            }
+            if (j==3 || j==6) {
+                tempRow[j].box.style.borderLeftColor = "#ff0000";
             }
             if (i==2 || i==5) {
-                tempRow[j].square.style.borderBottomColor = "#ff0000";
+                tempRow[j].box.style.borderBottomColor = "#ff0000";
             }
-            grid.appendChild(tempRow[j].square);
+            if (i==3 || i==6) {
+                tempRow[j].box.style.borderTopColor = "#ff0000";
+            }
+            grid.appendChild(tempRow[j].box);
         }
-        cells[i] = tempRow;
+        cell_grid[i] = tempRow;
     }
 }
 
 drawGrid(9);
+let cells = document.querySelectorAll('.square');
+cells.forEach((square) => {
+    square.addEventListener('click', ()=>{
+        for (let i = 0; i<9; i++){
+            for (let j = 0; j<9; j++) {
+                cell_grid[i][j].box.style.backgroundColor = 'rgb(255,255,255)'
+            }
+        }
+        square.style.backgroundColor = 'rgba(255,0,0,0.2)'
+    });
+});
